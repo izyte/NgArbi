98df0d712440bf18d92796d04e3cbba88a56abbd
Archangel <izyte@yahoo.com>
2020-01-23 16:58:50 +0800

    in the middle of working on inclusion of campaign field

--- src/app/svc/app.tables.ts
+++ src/app/svc/app.tables.ts
@@ -1,5 +1,5 @@
 /***********************************************************************
-* Automatically generated on 1/22/2020 6:47:51 AM
+* Automatically generated on 1/22/2020 7:39:13 PM
 ***********************************************************************/
 
 import { HttpClient } from '@angular/common/http';
